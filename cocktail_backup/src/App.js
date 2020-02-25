246bbe358c80bd04519617cba8c97daf3f1f65e0
Jannik Fischer <algebrator@Janniks-MacBook-Air.local>
2020-02-25 19:12:52 +0100

    nur aus Sicherheitsgründen, kein Code vorhanden

--- cocktail_backup/src/App.js
+++ cocktail_backup/src/App.js
@@ -11,9 +11,7 @@ function App(){
     return (
       <div>
         
-        <MainPage/>
-        <Match/>
-  
+       Hello world
         
       
       
--- cocktail_backup/src/components/selection/selecting.js
+++ cocktail_backup/src/components/selection/selecting.js
@@ -1,32 +1,5 @@
-import React from 'react';
 
 
 
 
-class Selecting extends React.Component{
-    constructor(props){
-        super(props)
-        
-        this.state ={ message:[]}
-}
-    
-    change=()=>{
-        this.state.message.push(this.props.item)
-            this.setState({
-                added: this.state.allItems
-            })
-        console.log(this.state.message)
-    }   
-render(){
-    return(
-        <div>
-            
-            {this.props.item} < button onClick ={() => this.change(this.state.message)}> <input type = "checkbox" className = "checkBox" ></input> </button>
-            <button onClick={() => (this.props.match(this.state.message))}> Einkaufen </button>
-        </div>
-    )
-    
 
-}
-}
-export default Selecting;
